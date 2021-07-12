89e67ddc59eb628de42c55df6d6b7e4fc9a5a177
Klaus Eckelt <Klaus.Eckelt@jku.at>
2021-07-12 14:38:37 +0200

    Create conda enviroment file

---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+import { JupyterFrontEndPlugin } from '@jupyterlab/application';
+/**
+ * Initialization data for the ml-prov extension.
+ */
+declare const plugin: JupyterFrontEndPlugin<void>;
+export default plugin;
