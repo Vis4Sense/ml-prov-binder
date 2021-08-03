6584061a5bc67ec85118682674da85183e6bf8ef
Klaus Eckelt <Klaus.Eckelt@jku.at>
2021-08-03 09:05:43 +0200

    format again #3

--- src/MlProvSidebar.ts
+++ src/MlProvSidebar.ts
@@ -1,21 +1,23 @@
 import { Widget } from '@lumino/widgets';
+import { Message } from '@lumino/messaging';
 import { D3BarChart } from './D3BarChart';
 import { offlineBoltIcon } from '@jupyterlab/ui-components';
 
 const TITLE = 'ML Prov';
 const DESCRIPTION = 'HyperParameters:';
 const values = '<li>h1 = 20</li><li>h2 = 10</li>';
-const hpInput = '<input size="15"> &nbsp; <button style="border-radius: 15px;">Add HP</button>';
+const hpInput =
+  '<input size="15"> &nbsp; <button style="border-radius: 15px;">Add HP</button>';
 const description2 = 'Performance Metrics:';
 const fScore = '<li>f-score: 0.672</li>';
-const metric = '<input size="15"> &nbsp; <button style="border-radius: 15px;">Add Metric</button>';
+const metric =
+  '<input size="15"> &nbsp; <button style="border-radius: 15px;">Add Metric</button>';
 
 /**
  * Sidebar widget to interact with the extension.
  * See https://github.com/jupyterlab/extension-examples/tree/master/widgets
  */
 export class MlProvSidebar extends Widget {
-
   /**
    * Returns a new table of contents.
    *
@@ -35,7 +37,9 @@ export class MlProvSidebar extends Widget {
   }
 
   addStaticDummyContent() {
-    this.node.insertAdjacentHTML('afterbegin', `
+    this.node.insertAdjacentHTML(
+      'afterbegin',
+      `
       <h1>${TITLE}</h1>
       <p>${DESCRIPTION}</p>
       <ul>${values}</ul>
@@ -45,25 +49,27 @@ export class MlProvSidebar extends Widget {
       <ul>${fScore}</ul>
       ${metric}
       <div style="margin-bottom: 1cm;"></div>
-    `);
+    `
+    );
   }
 
-  protected onBeforeShow(msg: any) {
+  protected onBeforeShow(msg: Message) {
     console.debug(`Opening ${TITLE} sidebar.`);
 
     //add new chart, different random data on every open
-    new D3BarChart([
-      { amount: Math.random() * 200 },
-      { amount: Math.random() * 200 },
-      { amount: Math.random() * 200 }
-    ], this.node);
+    new D3BarChart(
+      [
+        { amount: Math.random() * 200 },
+        { amount: Math.random() * 200 },
+        { amount: Math.random() * 200 }
+      ],
+      this.node
+    );
   }
 
-  protected onAfterHide(msg: any) {
+  protected onAfterHide(msg: Message) {
     console.debug(`closing ${TITLE} sidebar`);
     this.node.querySelector('svg.chart')?.remove();
     this.node.querySelector('svg.chart1')?.remove();
   }
 }
-
-
